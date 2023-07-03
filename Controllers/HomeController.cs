7f0e0eccced3ce92bf6221bdcbc216fed5162dd3
Dirk Mueller <dirkmuller8@outlook.de>
2023-07-03 14:00:04 +0200

    Completed RsvpForm.cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,7 +8,7 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
-        return View();
+        return RsvpForm();
     }
     public ViewResult RsvpForm()
     {
